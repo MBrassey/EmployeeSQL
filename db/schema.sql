ab53ee40b350e95471dada70249e5f6f29854a22
luc1dLife <luc1d_life@protonmail.com>
2020-10-20 00:16:14 -0600

    Corrected DB Connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
-DROP DATABASE IF EXISTS employeeSQL;
-CREATE DATABASE employeeSQL;
-USE DATABASE employeeSQL;
-DROP TABLE IF EXISTS employee;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS department;
+DROP DATABASE IF EXISTS `EmployeeSQL`;
+CREATE DATABASE if not EXISTS `EmployeeSQL`;
+USE `EmployeeSQL`; 
+DROP TABLE IF EXISTS `EmployeeSQL`.`employee`;
+DROP TABLE IF EXISTS `EmployeeSQL`.`roles`;
+DROP TABLE IF EXISTS `EmployeeSQL`.`department`;
 
-CREATE TABLE department (
+CREATE TABLE IF NOT EXISTS `EmployeeSQL`.`department` (
   id INT NOT NULL auto_increment PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-  id INT NOT NULL auto_increment PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-);
-
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS `EmployeeSQL`.`employee` (
   id INT NOT NULL auto_increment PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT
+);
+
+CREATE TABLE IF NOT EXISTS `EmployeeSQL`.`roles` (
+  id INT NOT NULL auto_increment PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
 );
