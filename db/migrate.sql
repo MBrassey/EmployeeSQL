6576c8944988e56c64176c8c906a095fe51aae1f
luc1dLife <luc1d_life@protonmail.com>
2020-10-20 21:56:36 -0600

    + View Functions

--- db/migrate.sql
+++ db/migrate.sql
@@ -2,29 +2,44 @@ DROP DATABASE IF EXISTS `EmployeeSQL`;
 CREATE DATABASE if not EXISTS `EmployeeSQL`;
 USE `EmployeeSQL`; 
 DROP TABLE IF EXISTS `EmployeeSQL`.`employee`;
-DROP TABLE IF EXISTS `EmployeeSQL`.`roles`;
+DROP TABLE IF EXISTS `EmployeeSQL`.`role`;
 DROP TABLE IF EXISTS `EmployeeSQL`.`department`;
 
-CREATE TABLE IF NOT EXISTS `EmployeeSQL`.`department` (
-  id INT NOT NULL auto_increment PRIMARY KEY,
+CREATE TABLE department (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `EmployeeSQL`.`employee` (
-  id INT NOT NULL auto_increment PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(16,2) NOT NULL,
+    department_id INTEGER ,
+    index department_id(department_id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    
 );
 
-CREATE TABLE IF NOT EXISTS `EmployeeSQL`.`roles` (
-  id INT NOT NULL auto_increment PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
-// Seed DATABASE
 INSERT INTO department(name)VALUES("Sales"), ("Engeneering"),("Fincance"), ("Legal");
 INSERT INTO role(title, salary, department_id)values("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 125000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Matt', 'Brassey', 1, null),
+('Vitalik', 'Buterin', 1, 1),
+('Jeff', 'Berwick', 2, 2),
+('Ron', 'Paul', 3, 1),
+('Ichigo', 'Kurosaki', 3, 2),
+('Bat', 'Man', 3, 1),
+('Son', 'Goku', 2, 2),
+('Bruce', 'Lee', 3, 1),
+('Sauske', 'Uchia', 4, null);
--- server.js
+++ server.js
@@ -1,16 +1,131 @@
 const connection = require("./db/database");
-const cTable = require("console-table");
 const inquirer = require("inquirer");
 const banner = require("./lib/banner");
 
-connection.connect(function (err) {
-  if (err) {
-    console.log(err);
-  }
-  console.log("connection id", connection.threadId);
-  init();
+connection.connect(function (err)
+{
+	if (err)
+	{
+		console.log(err);
+	}
+	//console.log("connection id", connection.threadId);  // If needed
+	init();
 });
 
-init = () => {
-  return banner;
+init = () =>
+{
+	banner;
+	return inquirer
+		.prompt(
+		{
+			name: 'initialAction',
+			type: 'list',
+			message: 'What would you like to do?',
+			choices: [
+				"View Departments",
+				"View Roles",
+				"View Employees",
+				"Add Department",
+				"Add Role",
+				"Add Employee",
+				"Update Employee's Role",
+				"Delete Employee",
+				"Exit"
+			]
+		})
+		.then(function (answer)
+		{
+			switch (answer.initialAction)
+			{
+			case "View Departments":
+				viewDepartments();
+				break;
+			case "View Roles":
+				viewRoles();
+				break;
+
+			case "View Employees":
+        viewEmployees();
+        init();
+				break;
+
+			case "Add Department":
+        addDepartment();
+				break;
+
+			case "Add Role":
+        addRole();
+				break;
+
+			case "Add Employee":
+				addEmployee();
+				break;
+
+			case "Update Employee's Role":
+				updateRole();
+				break;
+
+			case "Delete Employee":
+        deleteEmployee();
+				break;
+
+			case "Exit":
+				connection.end();
+			}
+		});
+}
+
+
+// View employees
+const viewEmployees = () =>
+{
+	const sql =
+		`SELECT e.id,
+     e.first_name,
+     e.last_name,
+     role.title,
+     department.name AS department,
+     role.salary,
+     CONCAT(e_manager.first_name, " ", e_manager.last_name) AS manager
+     FROM employee e
+     LEFT JOIN employee e_manager ON e.manager_id = e_manager.id
+     LEFT JOIN role ON e.role_id = role.id
+     LEFT JOIN department ON role.department_id = department.id`
+
+	connection.query(sql, function (err, res)
+	{
+		if (err) throw err;
+		for (i = 0; i < res.length; i++)
+		{
+			console.log("");
+      console.table(res[i]);
+		}
+	});
+};
+
+// View roles
+const viewRoles = () =>
+{
+	const roleQuery = `SELECT *, department.name as Department FROM role
+           LEFT JOIN department ON role.department_id = department.id
+           ORDER BY title; `
+	connection.query(roleQuery, function (err, results)
+	{
+		if (err) throw err;
+		console.log("");
+		console.table(results);
+	})
+	init();
+};
+
+// View departments
+const viewDepartments = () =>
+{
+	connection.query('SELECT * FROM department', function (err, results)
+	{
+		if (err) throw err;
+		console.log("");
+		console.table(results);
+  })
+  init();
 };
